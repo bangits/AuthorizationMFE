e2537cdc8971af70a36b0199b9cc27f99dd60027
Armen Mesropyan <amesropyan@bangits.com>
2023-04-27 15:03:57 +0400

    feat: added logic to enable/disable redux logger

--- src/adapter/redux/store/store.ts
+++ src/adapter/redux/store/store.ts
@@ -1,18 +1,20 @@
-import { configureStore, EnhancedStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import * as apiReducers from '../api';
 import rootReducer from './rootReducer';
 
-const createStore = (): EnhancedStore => {
-  const store = configureStore({
+const configLogger = () => (process.env.logger ? [logger] : []);
+
+const createStore = () => {
+  return configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      [...getDefaultMiddleware(), ...Object.values(apiReducers).map((api) => api.middleware), logger] as ReturnType<
-        typeof getDefaultMiddleware
-      >
+      [
+        ...getDefaultMiddleware(),
+        ...Object.values(apiReducers).map((api) => api.middleware),
+        ...configLogger()
+      ] as ReturnType<typeof getDefaultMiddleware>
   });
-
-  return store;
 };
 
 export default createStore;
